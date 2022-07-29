82f380b0159a1fab710b81931c27765fdc1afc15
Dan Hable <dan@danhable.com>
2022-07-29 11:57:40 -0500

    doc: Add rustdoc comments for Option and Result impl
    Fixes #4

--- merge-rs/src/monad.rs
+++ merge-rs/src/monad.rs
@@ -1,5 +1,17 @@
 use crate::Merge;
 
+/// Implementation of merge for the Option type. The merge is defined with the following
+/// logic chart:
+///
+/// | Target  | Other   | Result           |
+/// |---------|---------|------------------|
+/// | None    | None    | None             |
+/// | Some(a) | None    | Some(a)          |
+/// | None    | Some(b) | Some(b)          |
+/// | Some(a) | Some(b) | Some(a.merge(b)) |
+///
+/// Two Option instances can only merge if their containing data elements also support
+/// merging.
 impl<T: Clone + Merge> Merge for Option<T> {
     fn merge(&self, rhs: &Self) -> Self {
         match (self, rhs) {
@@ -11,12 +23,16 @@ impl<T: Clone + Merge> Merge for Option<T> {
     }
 }
 
+/// Implementation of merge for the Result type. The merge strategy used is right-bias
+/// towards the error case. If the target or right hand side is an [Err] instance, the
+/// result will contain that [Err] value. When both values are [Ok], then the result will
+/// be an [Ok] containing the result of merging the inner values.
 impl<T: Clone + Merge, E: Clone + Merge> Merge for Result<T, E> {
     fn merge(&self, rhs: &Self) -> Self {
         match (self, rhs) {
             (Err(left), _) => Err(left.clone()),
             (_, Err(right)) => Err(right.clone()),
-            (Ok(left), Ok(right)) => Ok(left.merge(right))
+            (Ok(left), Ok(right)) => Ok(left.merge(right)),
         }
     }
 }
