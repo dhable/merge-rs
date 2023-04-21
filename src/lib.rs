8e9080aa58d2d9b88c21b49c3de0891476399ae1
Dan Hable <dan@danhable.com>
2023-04-21 10:34:48 -0500

    feat(macro): switch to derive macro approach
    Implement derive macros for each merge trait in the library. This
    replaces the `Mergable` wrapper type approach from before.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 /// Trait for types that support merging two values and producing a new value
 /// with the result of the merge. The input values are left unchanged.
 pub trait Merge {
-    #[must_use = "Did you want to implement MergeMut instead?"]
-    fn merge(&self, other: &Self) -> Self;
+    fn merge(&self, other: &Self) -> Result<Self, Box<dyn std::error::Error>> where Self: Sized;
 }
 
 /// Trait for types that support merging two values and replace the target values
 /// with the result of the merge operation.
 pub trait MergeMut {
-    fn merge_mut(&mut self, other: &Self);
+    fn merge_mut(&mut self, other: &Self) -> Result<(), Box<dyn std::error::Error>>;
 }
-
-mod collections;
-pub mod mergeable;
-mod monad;
