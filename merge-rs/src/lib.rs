61cd5a14136b02dec7b1a608f4e06cd23f9505ee
Dan Hable <dan@danhable.com>
2023-04-21 10:46:25 -0500

    fix: emit warning if Merge result is not used
    Fixes issue #16.

--- merge-rs/src/lib.rs
+++ merge-rs/src/lib.rs
@@ -1,6 +1,7 @@
 /// Trait for types that support merging two values and producing a new value
 /// with the result of the merge. The input values are left unchanged.
 pub trait Merge {
+    #[must_use = "Did you want to implement MergeMut instead?"]
     fn merge(&self, other: &Self) -> Self;
 }
 
